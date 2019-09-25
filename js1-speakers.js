c1802e191bd28182042e2ae84e40190249d8a76e
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-09-25 18:29:03 +0200

    add tw handles

--- js1-speakers.js
+++ js1-speakers.js
@@ -4,6 +4,7 @@ module.exports = [
         name: 'Mauricio Palma',
         topic: 'Accessibility vs latest Web APIs. Can’t we just get along?',
         avatar: 'https://jsconfbp.com/static/c352604e07847b50b1eae55e6a54a11c/71b1f/mauricio_palma.webp',
+        twitter: '@palmaswell',
         color: 'blue',
     },
     {
@@ -11,6 +12,7 @@ module.exports = [
         name: 'Shelley Vohr',
         topic: 'API Modernization: Building Bridges As You Cross Them',
         avatar: 'https://jsconfbp.com/static/f8ccbef0a81b2eb61c2f8f114132b777/5e095/shelley_vohr.webp',
+        twitter: '@codebytere',
         color: 'green',
     },
     {
@@ -18,6 +20,7 @@ module.exports = [
         name: 'Jorge Marin',
         topic: 'Testing in production: Ideas, experiences, limits, roadblocks',
         avatar: 'https://jsconfbp.com/static/6f266645f340d0074c830e4fd1186a3f/340a4/jorge_marin.webp',
+        twitter: '@chipironcin',
         color: 'yellow',
     },
     {
@@ -29,6 +32,7 @@ module.exports = [
         name: 'Charlie Gerard',
         topic: 'Weaving the web - Programming textile-based interactions in JavaScript',
         avatar: 'https://jsconfbp.com/static/f22435d480c1f809a1dc433e428aa7cd/5e095/charlie_gerard.webp',
+        twitter: '@devdevcharlie',
         color: 'green',
     },
     {
@@ -36,6 +40,7 @@ module.exports = [
         name: 'Taylor Fairbank',
         topic: 'Javascript Performance in Extreme Conditions: Building WebApps for the Refugee Aid Movement',
         avatar: 'https://jsconfbp.com/static/5cb3e6dab890b99cef8ec4b1e2260cf8/5ddc8/taylor_fairbank.webp',
+        twitter: '',
         color: 'green',
     },
     {
@@ -47,6 +52,7 @@ module.exports = [
         name: 'Adam Giese',
         topic: 'Composing music with composed functions',
         avatar: 'https://jsconfbp.com/static/efe4d5b8263d4c3b9aca9e4e13a55149/6fae7/adam_giese.webp',
+        twitter: '@AdamGieseDev',
         color: 'red',
     },
     {
@@ -54,6 +60,7 @@ module.exports = [
         name: 'Rebecca Hill',
         topic: 'Essential JavaScript debugging tools for the modern detective',
         avatar: 'https://jsconfbp.com/static/a9c029c4c6eecffca2b88d483a572cd5/80349/rebecca_hill.webp',
+        twitter: '@rebekaka',
         color: 'green',
     },
     {
@@ -65,6 +72,7 @@ module.exports = [
         name: 'Stephanie Nemeth',
         topic: 'How not to read the room: Creating socially awkward wearables with machine learning & javascript',
         avatar: 'https://jsconfbp.com/static/d904ffbefa15ca047e46190119f8c67d/1508f/stephanie_nemeth.webp',
+        twitter: '@stephaniecodes',
         color: 'black',
     },
     {
@@ -72,6 +80,7 @@ module.exports = [
         name: 'Damini Satya Kammakomati',
         topic: 'Taming “Git”osaurus Using Mystical Trees: Understanding complex git trees in the developer fairyland',
         avatar: 'https://jsconfbp.com/static/9e7e5271243bef3c85055651a32e8ce5/f4957/damini_satya.webp',
+        twitter: '@Daminisatya',
         color: 'white',
     },
     {
@@ -79,6 +88,7 @@ module.exports = [
         name: 'Jake Archibald & Surma',
         topic: 'HTTP 203 Unplugged',
         avatar: 'https://jsconfbp.com/static/91bcd7c05917c76597769de2f1c380d7/1508f/jake_surma.webp',
+        twitter: '@DasSurma & @jaffathecake',
         color: 'black',
     },
 ]
