890d06253293aa98c00e38a72f32e679c5d88987
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2022-04-14 16:38:09 +0200

    Release (#24)
    * Update js2-speakers.js
    
    * lirian blue -> purple
    
    * Update index.js
    
    Co-authored-by: Nagy Gergo <nagy.gergo@3pixel.hu>

--- js2-speakers.js
+++ js2-speakers.js
@@ -1,94 +1,105 @@
 module.exports = [
-    {
-        id: 'kat',
-        name: 'Kat Kitay',
-        topic: 'Web Norms of the World: An exploration of the internet beyond the West',
-        avatar: 'https://jsconfbp.com/static/cb4f8d0b1d67a4c680fe828179907f11/42a0f/kat_kitay.webp',
-        twitter: '@kkitay',
-        color: 'purple',
-    },
-    {
-        id: 'tejas',
-        name: 'Tejas Kumar',
-        topic: 'Legendary Lambdas',
-        avatar: 'https://jsconfbp.com/static/43ebbec38428deed3f2389a45be98e7d/ae483/tejas_kumar.webp',
-        twitter: '@tejaskumar_',
-        color: 'red',
-    },
-    {
-        id: 'ruben',
-        name: 'Rubén Sospedra',
-        topic: 'Mastering UIs with Finite State Machines',
-        avatar: 'https://jsconfbp.com/static/e6e071744b26aaac76c9797a684de6b8/f4957/ruben_sospedra.webp',
-        twitter: '@sospedra_r',
-        color: 'yellow',
-    },
-    {
-		break: true,
-		name: 'Coffee break'
-	},
-    {
-        id: 'braden',
-        name: 'Braden Moore',
-        topic: 'Deciphering Brainwaves with the Web Audio API',
-        avatar: 'https://jsconfbp.com/static/3dd5b50dd8d463e5db3231acc85db9a1/c6096/braden_moore.webp',
-        twitter: '@braden_rm',
-        color: 'blue',
-    },
-    {
-        id: 'benedicte',
-        name: 'Benedicte Raae',
-        topic: 'A privacy first period tracker? Is it even possible?',
-        avatar: 'https://jsconfbp.com/static/182c63ad78e72fbc721ae000ce877af0/c6096/benedicte_raae.webp',
-        twitter: '@raae',
-        color: 'yellow',
-    },
-    {
-		break: true,
-		name: 'Lunch break'
-	},
-    {
-        id: 'eva',
-        name: 'Eva Ferreira',
-        topic: 'Take on me, web browsers!',
-        avatar: 'https://jsconfbp.com/static/d737546b2c2ce8f3136cf747a2201b8b/7a72d/eva_ferreira.webp',
-        twitter: '@evaferreira92',
-        color: 'orange',
-    },
-    {
-        id: 'liran',
-        name: 'Liran Tal',
-        topic: 'StrangerDanger: Finding Security Vulnerabilities Before They Find You!',
-        avatar: 'https://jsconfbp.com/static/c1a3ce9c825af12e420d8756444f1f6c/7a72d/liran_tal.webp',
-        twitter: '@liran_tal',
-        color: 'blue',
-    },
-    {
-		break: true,
-		name: 'Coffee break'
-	},
-    {
-        id: 'balazs',
-        name: 'Korossy-Khayll, Balázs',
-        topic: 'Testing presentation components visually',
-        avatar: 'https://jsconfbp.com/static/81d2332a6afab80cb2b7d83036c2ee8a/7a72d/balazs_korossy_khayll.webp',
-        twitter: '@bkhayll',
-        color: 'white',
-    },
-    {
-        id: 'vitalii',
-        name: 'Vitalii Bobrov',
-        topic: 'Algorithms and Their Habitat',
-        avatar: 'https://jsconfbp.com/static/2ee0d93d1e9d766a2758d5e340ea351a/7a72d/vitalii_bobrov.webp',
-        twitter: '@bobrov1989',
-        color: 'green',
-    },
-    {
-        id: 'isa',
-        name: 'Isa Silveira',
-        topic: 'Looking under the rug: the art of learning from failure',
-        avatar: 'https://jsconfbp.com/static/f6374502c504eb6d34c71099a97f966c/05a89/isa_silveira.webp',
-        twitter: '@silveira_bells',
-        color: 'red',
-    }
-]
+  {
+    id: 'kat',
+    name: 'Kat Kitay',
+    topic: 'Web Norms of the World',
+    avatar:
+      'https://jsconfbp.com/static/cb4f8d0b1d67a4c680fe828179907f11/42a0f/kat_kitay.webp',
+    twitter: '@kkitay',
+    color: 'purple'
+  },
+  {
+    id: 'tejas',
+    name: 'Tejas Kumar',
+    topic: 'Legendary Lambdas',
+    avatar:
+      'https://jsconfbp.com/static/43ebbec38428deed3f2389a45be98e7d/ae483/tejas_kumar.webp',
+    twitter: '@tejaskumar_',
+    color: 'red'
+  },
+  {
+    id: 'ruben',
+    name: 'Rubén Sospedra',
+    topic: 'Mastering UIs with Finite State Machines',
+    avatar:
+      'https://jsconfbp.com/static/e6e071744b26aaac76c9797a684de6b8/f4957/ruben_sospedra.webp',
+    twitter: '@sospedra_r',
+    color: 'yellow'
+  },
+  {
+    break: true,
+    name: 'Coffee break'
+  },
+  {
+    id: 'braden',
+    name: 'Braden Moore',
+    topic: 'Deciphering Brainwaves with the Web Audio API',
+    avatar:
+      'https://jsconfbp.com/static/3dd5b50dd8d463e5db3231acc85db9a1/c6096/braden_moore.webp',
+    twitter: '@braden_rm',
+    color: 'blue'
+  },
+  {
+    id: 'benedicte',
+    name: 'Benedicte Raae',
+    topic: 'A privacy first period tracker? Is it even possible?',
+    avatar:
+      'https://jsconfbp.com/static/182c63ad78e72fbc721ae000ce877af0/c6096/benedicte_raae.webp',
+    twitter: '@raae',
+    color: 'yellow'
+  },
+  {
+    break: true,
+    name: 'Lunch break'
+  },
+  {
+    id: 'eva',
+    name: 'Eva Ferreira',
+    topic: 'Take on me, web browsers!',
+    avatar:
+      'https://jsconfbp.com/static/d737546b2c2ce8f3136cf747a2201b8b/7a72d/eva_ferreira.webp',
+    twitter: '@evaferreira92',
+    color: 'orange'
+  },
+  {
+    id: 'liran',
+    name: 'Liran Tal',
+    topic:
+      'StrangerDanger: Finding Security Vulnerabilities Before They Find You!',
+    avatar:
+      'https://jsconfbp.com/static/c1a3ce9c825af12e420d8756444f1f6c/7a72d/liran_tal.webp',
+    twitter: '@liran_tal',
+    color: 'purple'
+  },
+  {
+    break: true,
+    name: 'Coffee break'
+  },
+  {
+    id: 'balazs',
+    name: 'Korossy-Khayll, Balázs',
+    topic: 'Testing presentation components visually',
+    avatar:
+      'https://jsconfbp.com/static/81d2332a6afab80cb2b7d83036c2ee8a/7a72d/balazs_korossy_khayll.webp',
+    twitter: '@bkhayll',
+    color: 'white'
+  },
+  {
+    id: 'vitalii',
+    name: 'Vitalii Bobrov',
+    topic: 'Algorithms and Their Habitat',
+    avatar:
+      'https://jsconfbp.com/static/2ee0d93d1e9d766a2758d5e340ea351a/7a72d/vitalii_bobrov.webp',
+    twitter: '@bobrov1989',
+    color: 'green'
+  },
+  {
+    id: 'isa',
+    name: 'Isa Silveira',
+    topic: 'Looking under the rug: the art of learning from failure',
+    avatar:
+      'https://jsconfbp.com/static/f6374502c504eb6d34c71099a97f966c/05a89/isa_silveira.webp',
+    twitter: '@silveira_bells',
+    color: 'red'
+  }
+];
